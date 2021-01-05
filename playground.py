f3eb52b777974d2340fb8ea62e7771615be2fb9e
James <james.quacinella@gmail.com>
2021-01-04 23:58:26 -0500

    Updating file with all code, better imports and updates to using proper strip() function. However, without self._ia which is some conneciton to IMDB, we cannot do more than just import ing the movies

--- playground.py
+++ playground.py
@@ -1,110 +1,130 @@
-def import_movies(self, file):
-    """
-    Page 161
-    """
-    with open(file, 'r+') as in_file:
-        reader = csv.reader(in_file, delimiter=',') #A next(reader, None)
-    with self._driver.session() as session: #B
-        session.run(
-        "CREATE CONSTRAINT ON (a:Movie) ASSERT a.movieId IS UNIQUE; ") #C
-        session.run(
-        "CREATE CONSTRAINT ON (a:Genre) ASSERT a.genre IS UNIQUE; ") #C
-        tx = session.begin_transaction() #D
-        i = 0;
-        j = 0;
-        for row in reader:
-            try:
-                if row:
-                    movie_id = strip(row[0]) title = strip(row[1]) genres = strip(row[2]) query = """ #E
-                    CREATE (movie:Movie {movieId: {movieId}, title: {title}}) with movie
-                    UNWIND {genres} as genre
-                    MERGE (g:Genre {genre: genre})
-                    MERGE (movie)-[:HAS_GENRE]->(g)
-                    """
-                    tx.run(query, {"movieId": movie_id, "title": title, "genres":
-                    genres.split("|")})
-                    i += 1
-                    j += 1
-                if i == 1000: #F tx.commit()
-                    print(j, "lines processed") i=0
-                    tx = session.begin_transaction()
-            except Exception as e:
-                print(e, row, reader.line_num)
-        tx.commit()
-        print(j, "lines processed")
+from neo4j import GraphDatabase
+import csv
 
-def import_movie_details(self, file):
-    with open(file, 'r+') as in_file:
-        reader = csv.reader(in_file, delimiter=',')
-        next(reader, None)
-        with self._driver.session() as session:
-            session.run("CREATE CONSTRAINT ON (a:Person) ASSERT a.name IS UNIQUE;") #A tx = session.begin_transaction()
-            i = 0;
-            j = 0;
-            for row in reader:
-                try:
-                    if row:
-                        movie_id = strip(row[0]) imdb_id = strip(row[1])
-                        movie = self._ia.get_movie(imdb_id) #B self.process_movie_info(movie_info=movie, tx=tx, movie_id=movie_id) #C i += 1
-                        j += 1
-                    if i == 10: 
-                        tx.commit()
-                        print(j, "lines processed")
-                        i=0
-                        tx = session.begin_transaction()
-                except Exception as e:
-                    print(e, row, reader.line_num)
-            tx.commit()
-            print(j, "lines processed")
+class MovieExample:
 
-
-def process_movie_info(self, tx, movie_info, movie_id):
-    query = """
-        MATCH (movie:Movie {movieId: {movieId}})
-        SET movie.plot = {plot}
-        FOREACH (director IN {directors} | MERGE (d:Person {name: director}) SET d:Director
-        MERGE (d)-[:DIRECTED]->(movie))
-        FOREACH (actor IN {actors} | MERGE (d:Person {name: actor}) SET d:Actor MERGE (d)-
-        [:ACTS_IN]->(movie))
-        FOREACH (producer IN {producers} | MERGE (d:Person {name: producer}) SET d:Producer
-        MERGE (d)-[:PRODUCES]->(movie))
-        FOREACH (writer IN {writers} | MERGE (d:Person {name: writer}) SET d:Writer MERGE
-        (d)-[:WRITES]->(movie))
-        FOREACH (genre IN {genres} | MERGE (g:Genre {genre: genre}) MERGE (movie)-
-        [:HAS_GENRE]->(g)) """
-    directors = []
-    for director in movie_info['directors']:
-        if 'name' in director.data:
-            directors.append(director['name'])
-    genres = ''
-    if 'genres' in movie_info:
-        genres = movie_info['genres']
-    actors = []
-    for actor in movie_info['cast']:
-        if 'name' in actor.data: 
-            actors.append(actor['name'])
-    writers = []
-    for writer in movie_info['writers']:
-        if 'name' in writer.data:
-            writers.append(writer['name'])
-    producers = []
-    for producer in movie_info['producers']:
-        producers.append(producer['name'])
-    plot = '' #E
-    if 'plot outline' in movie_info:
-        plot = movie_info['plot outline']
-    tx.run(query, {"movieId": movie_id, "directors": directors, "genres": genres, "actors": actors, "plot": plot,
-    "writers": writers, "producers": producers})
-
-
-def import_user_item(self, file):
-    """
-    from page 169
-    """
-    with open(file, 'r+') as in_file:
-        reader = csv.reader(in_file, delimiter=',') next(reader, None)
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+            
+    def import_movies(self, file):
+        with open(file, 'r+') as in_file:
+            reader = csv.reader(in_file, delimiter=',')
+            next(reader, None)
             with self._driver.session() as session:
-                session.run("CREATE CONSTRAINT ON (u:User) ASSERT u.userId IS UNIQUE") #A
+                # session.run(
+                #     "CREATE CONSTRAINT ON (a:Movie) ASSERT a.movieId IS UNIQUE; ")
+                # session.run(
+                #     "CREATE CONSTRAINT ON (a:Genre) ASSERT a.genre IS UNIQUE; ")
+    
+                tx = session.begin_transaction()
+    
+                i = 0;
+                j = 0;
+                for row in reader:
+                    try:
+                        if row:
+                            movie_id = row[0].strip()
+                            title = row[1].strip()
+                            genres = row[2].strip()
+                            query = """
+                                CREATE (movie:Movie {movieId: $movieId, title: $title})
+                                with movie
+                                UNWIND $genres as genre
+                                MERGE (g:Genre {genre: genre})
+                                MERGE (movie)-[:HAS_GENRE]->(g)
+                            """
+                            tx.run(query, {"movieId": movie_id, "title": title, "genres": genres.split("|")})
+                            i += 1
+                            j += 1
+    
+                        if i == 1000:
+                            tx.commit()
+                            print(j, "lines processed")
+                            i = 0
+                            tx = session.begin_transaction()
+                    except Exception as e:
+                        print(e, row, reader.line_num)
+                tx.commit()
+                print(j, "lines processed")
+
+
+    def import_movie_details(self, file):
+        with open(file, 'r+') as in_file:
+            reader = csv.reader(in_file, delimiter=',')
+            next(reader, None)
+            with self._driver.session() as session:
+                session.run("CREATE CONSTRAINT ON (a:Person) ASSERT a.name IS UNIQUE;")
+                tx = session.begin_transaction()
+                i = 0;
+                j = 0;
+                for row in reader:
+                    try:
+                        if row:
+                            movie_id = row[0].strip()
+                            imdb_id = row[1].strip()
+                            movie = self._ia.get_movie(imdb_id)
+                            self.process_movie_info(movie_info=movie, tx=tx, movie_id=movie_id)
+                            i += 1
+                            j += 1
+
+                        if i == 10:
+                            tx.commit()
+                            print(j, "lines processed")
+                            i = 0
+                            tx = session.begin_transaction()
+                    except Exception as e:
+                        print(e, row, reader.line_num)
+                tx.commit()
+                print(j, "lines processed")
+    
+    def process_movie_info(self, tx, movie_info, movie_id):
+        query = """
+            MATCH (movie:Movie {movieId: {movieId}})
+            SET movie.plot = {plot}
+            FOREACH (director IN {directors} | MERGE (d:Person {name: director}) SET d:Director MERGE (d)-[:DIRECTED]->(movie))
+            FOREACH (actor IN {actors} | MERGE (d:Person {name: actor}) SET d:Actor MERGE (d)-[:ACTS_IN]->(movie))
+            FOREACH (producer IN {producers} | MERGE (d:Person {name: producer}) SET d:Producer MERGE (d)-[:PRODUCES]->(movie))
+            FOREACH (writer IN {writers} | MERGE (d:Person {name: writer}) SET d:Writer MERGE (d)-[:WRITES]->(movie))
+            FOREACH (genre IN {genres} | MERGE (g:Genre {genre: genre}) MERGE (movie)-[:HAS_GENRE]->(g))
+        """
+        directors = []
+        for director in movie_info['directors']:
+            if 'name' in director.data:
+                directors.append(director['name'])
+    
+        genres = ''
+        if 'genres' in movie_info:
+            genres = movie_info['genres']
+    
+        actors = []
+        for actor in movie_info['cast']:
+            if 'name' in actor.data:
+                actors.append(actor['name'])
+    
+        writers = []
+        for writer in movie_info['writers']:
+            if 'name' in writer.data:
+                writers.append(writer['name'])
+    
+        producers = []
+        for producer in movie_info['producers']:
+            producers.append(producer['name'])
+    
+        plot = ''
+        if 'plot outline' in movie_info:
+            plot = movie_info['plot outline']
+    
+        tx.run(query, {"movieId": movie_id, "directors": directors, "genres": genres, "actors": actors, "plot": plot,
+                    "writers": writers, "producers": producers})
+
+
+    def import_user_item(self, file):
+        with open(file, 'r+') as in_file:
+            reader = csv.reader(in_file, delimiter=',')
+            next(reader, None)
+            with self._driver.session() as session:
+                session.run("CREATE CONSTRAINT ON (u:User) ASSERT u.userId IS UNIQUE")
+
                 tx = session.begin_transaction()
                 i = 0;
                 for row in reader:
@@ -115,18 +135,89 @@ def import_user_item(self, file):
                             rating = strip(row[2])
                             timestamp = strip(row[3])
                             query = """
-                            MATCH (movie:Movie {movieId: {movieId}})
-                            MERGE (user:User {userId: {userId}})
-                            MERGE (user)-[:RATES {rating: {rating}, timestamp: {timestamp}}]->movie
+                                MATCH (movie:Movie {movieId: {movieId}})
+                                MERGE (user:User {userId: {userId}})
+                                MERGE (user)-[:RATES {rating: {rating}, timestamp: {timestamp}}]->(movie)
                             """
-                            tx.run(query, {"movieId":movie_id, 
-                                            "userId": user_id,
-                                            "rating":rating, 
-                                            "timestamp": timestamp}
-                                            )
+                            tx.run(query, {"movieId":movie_id, "userId": user_id, "rating":rating, "timestamp": timestamp})
                             i += 1
-                        if i == 1000: tx.commit()
-                            i=0
-                            tx = session.begin_transaction() except Exception as e:
-                            print(e, row, reader.line_num) tx.commit()
+                        if i == 1000:
+                            tx.commit()
+                            i = 0
+                            tx = session.begin_transaction()
+                    except Exception as e:
+                        print(e, row, reader.line_num)
+                tx.commit()
 
+    def recommendTo(self, userId, k):
+        user_VSM = self.get_user_vector(userId)
+        movies_VSM = self.get_movie_vectors(userId)
+        top_k = self.compute_top_k (user_VSM, movies_VSM, k);
+        return top_k
+    
+    def compute_top_k(self, user, movies, k):
+        dtype = [ ('movieId', 'U10'),('value', 'f4')]
+        knn_values = np.array([], dtype=dtype)
+        for other_movie in movies:
+            value = cosine_similarity([user], [movies[other_movie]])
+            if value > 0:
+                knn_values = np.concatenate((knn_values, np.array([(other_movie, value)], dtype=dtype)))
+        knn_values = np.sort(knn_values, kind='mergesort', order='value' )[::-1]
+        return np.array_split(knn_values, [k])[0]
+    
+    def get_user_vector(self, user_id):
+        query = """
+                    MATCH p=(user:User)-[:WATCHED|:RATES]->(movie)
+                    WHERE user.userId = {userId}
+                    with count(p) as total
+                    MATCH (feature:Feature)
+                    WITH feature, total
+                    ORDER BY id(feature)
+                    MATCH (user:User)
+                    WHERE user.userId = {userId}
+                    OPTIONAL MATCH (user)-[r:INTERESTED_IN]-(feature)
+                    WITH CASE WHEN r IS null THEN 0 ELSE (r.weight*1.0f)/(total*1.0f) END as value
+                    RETURN collect(value) as vector
+                """
+        user_VSM = None
+        with self._driver.session() as session:
+            tx = session.begin_transaction()
+            vector = tx.run(query, {"userId": user_id})
+            user_VSM = vector.single()[0]
+        print(len(user_VSM))
+        return user_VSM;
+    
+    def get_movie_vectors(self, user_id):
+        list_of_moview_query = """
+                    MATCH (movie:Movie)-[r:DIRECTED|:HAS_GENRE]-(feature)<-[i:INTERESTED_IN]-(user:User {userId: {userId}})
+                    WHERE NOT EXISTS((user)-[]->(movie)) AND EXISTS((user)-[]->(feature))
+                    WITH movie, count(i) as featuresCount
+                    WHERE featuresCount > 5
+                    RETURN movie.movieId as movieId
+                """
+    
+        query = """
+                    MATCH (feature:Feature)
+                    WITH feature
+                    ORDER BY id(feature)
+                    MATCH (movie:Movie)
+                    WHERE movie.movieId = {movieId}
+                    OPTIONAL MATCH (movie)-[r:DIRECTED|:HAS_GENRE]-(feature)
+                    WITH CASE WHEN r IS null THEN 0 ELSE 1 END as value
+                    RETURN collect(value) as vector;
+                """
+        movies_VSM = {}
+        with self._driver.session() as session:
+            tx = session.begin_transaction()
+    
+            i = 0
+            for movie in tx.run(list_of_moview_query, {"userId": user_id}):
+                movie_id = movie["movieId"];
+                vector = tx.run(query, {"movieId": movie_id})
+                movies_VSM[movie_id] = vector.single()[0]
+                i += 1
+                if i % 100 == 0:
+                    print(i, "lines processed")
+            print(i, "lines processed")
+        print(len(movies_VSM))
+        return movies_VSM
